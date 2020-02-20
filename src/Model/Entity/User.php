aca488e02c93e437629a269e19147fbb49bae003
nbertin <nancy.bertin@irisa.fr>
2020-02-20 19:51:37 +0100

    Add auth to User entity

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -63,12 +63,10 @@ class User extends Entity
         'password',
     ];
 
-    /**
-    * protected function _setPassword(string $password) : ?string
-    * {
-    *    if (strlen($password) > 0) {
-    *        return (new DefaultPasswordHasher())->hash($password);
-    *    }
-    * }
-    */
+    protected function _setPassword(string $password) : ?string
+    {
+      if (strlen($password) > 0) {
+         return (new DefaultPasswordHasher())->hash($password);
+      }
+    }
 }
