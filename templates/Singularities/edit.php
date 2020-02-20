119a766f52652d4b86dcf14d177afe25d7e31ea4
nbertin <nancy.bertin@irisa.fr>
2020-02-20 15:59:10 +0100

    Add template for singularity edition

---
+++ templates/Singularities/edit.php
@@ -0,0 +1,15 @@
+<!-- File: templates/Singularities/edit.php -->
+
+<h1>Edit Singularity</h1>
+<?php
+    echo $this->Form->create($singularity);
+    // Hard code the user for now.
+    echo $this->Form->control('user_id', ['type' => 'hidden', 'value' => 1]);
+    echo $this->Form->control('id');
+    // echo $this->Form->control('body', ['rows' => '3']);
+    echo $this->Form->control('name_fr');
+    echo $this->Form->control('name_en');
+    echo $this->Form->control('picture');
+    echo $this->Form->button(__('Save Singularity'));
+    echo $this->Form->end();
+?>
