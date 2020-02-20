448b1786317503d9e60667a034c8e24669de0841
nbertin <nancy.bertin@irisa.fr>
2020-02-20 12:02:56 +0100

    Add add() to Singularities controller

--- src/Controller/SingularitiesController.php
+++ src/Controller/SingularitiesController.php
@@ -5,6 +5,15 @@ namespace App\Controller;
 
 class SingularitiesController extends AppController
 {
+
+  public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash'); // Include the FlashComponent
+    }
+
   public function index()
     {
         $this->loadComponent('Paginator');
@@ -16,5 +25,24 @@ class SingularitiesController extends AppController
     {
         $singularity = $this->Singularities->findById($id)->firstOrFail();
         $this->set(compact('singularity'));
+    }
+
+    public function add()
+    {
+        $article = $this->Singularities->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Singularities->patchEntity($singularity, $this->request->getData());
+
+            // Hardcoding the user_id is temporary, and will be removed later
+            // when we build authentication out.
+            $singularity->user_id = 1;
+
+            if ($this->Singularities->save($singularity)) {
+                $this->Flash->success(__('Your singularity has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your singularity.'));
+        }
+        $this->set('singularity', $singularity);
     }
 }
