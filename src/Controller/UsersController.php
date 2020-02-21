c091513cd08637b310a592c0278327c59b95e63c
nbertin <nancy.bertin@irisa.fr>
2020-02-21 12:51:21 +0100

    Add logout

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -136,4 +136,14 @@ class UsersController extends AppController
             $this->Flash->error(__('Invalid username or password'));
         }
     }
+
+    public function logout()
+    {
+        $result = $this->Authentication->getResult();
+        // regardless of POST or GET, redirect if user is logged in
+        if ($result->isValid()) {
+            $this->Authentication->logout();
+            return $this->redirect(['controller' => 'Users', 'action' => 'login']);
+        }
+    }
 }
