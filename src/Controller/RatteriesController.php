07c25e2403c41688d7c78a1abacd836040b2a5ec
VigiePirate <artefact@vigies-pirates.net>
2020-03-03 21:41:41 +0100

    Removed plural...

--- src/Controller/RatteriesController.php
+++ src/Controller/RatteriesController.php
@@ -127,13 +127,13 @@ class RatteriesController extends AppController
 
         // Use the ArticlesTable to find tagged articles.
         $articles = $this->Ratteries->find('prefixed', [
-            'prefixes' => $prefixes
+            'prefixes' => $prefix
         ]);
 
         // Pass variables into the view template context.
         $this->set([
             'ratteries' => $ratteries,
-            'prefixes' => $prefixes
+            'prefixes' => $prefix
         ]);
     }
 
