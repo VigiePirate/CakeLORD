c4c3572795cb7fe84223dc09756a743bd538d49c
VigiePirate <artefact@vigies-pirates.net>
2020-03-08 20:31:13 +0100

    Format date input

--- src/Controller/RatsController.php
+++ src/Controller/RatsController.php
@@ -266,7 +266,8 @@ class RatsController extends AppController
     {
         // The 'pass' key is provided by CakePHP and contains all
         // the passed URL path segments in the request.
-        $bornBefore = $this->request->getParam('pass');
+        $bornBeforeString = $this->request->getParam('pass');
+        $bornBefore = new Chronos($bornBeforeString . '00:00:00.000');
         //
         // Use the RatsTable to find named rats.
         $rats = $this->Rats->find('bornBefore', [
