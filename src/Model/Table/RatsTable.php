00bd3af1ec4c10feb7f51a4b8870e09b392d984b
VigiePirate <artefact@vigies-pirates.net>
2020-03-08 21:24:50 +0100

    query

--- src/Model/Table/RatsTable.php
+++ src/Model/Table/RatsTable.php
@@ -362,7 +362,8 @@ class RatsTable extends Table
             // Find rats with birthdates before passed parameter
             // $bornBefore = ($options['bornBefore']) . " 00:00:00.000";
             $query->where([
-                    'Rats.birth_date <=' => "STR_TO_DATE(".($options['bornBefore']).")",
+                    //'Rats.birth_date <=' => "STR_TO_DATE(".($options['bornBefore']).")",
+                    'Rats.birth_date <=' => ($options['bornBefore']),
             ]);
         }
 
