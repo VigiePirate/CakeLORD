76efdb3c71e5c02ddd293399e90c6c2bf9f27ccc
VigiePirate <artefact@vigies-pirates.net>
2020-03-22 17:59:50 +0100

    Idem

--- templates/Users/reset_password.php
+++ templates/Users/reset_password.php
@@ -1,13 +1,11 @@
-<!-- copied from a cake 3 tutorial, to be fixed -->
-
 <?= $this->Flash->render() ?>
 
 <div class="users form index content">
     <?= $this->Form->create($user) ?>
     <fieldset>
-        <legend><?= echo __('Enter New Password') ?></legend>
+        <legend><?= __('Enter New Password') ?></legend>
         <?= $this->Form->control('password', ['required' => true, 'autofocus' => true]) ?>
-        <p class="helper">We recommend you to chose a password ofat least 12 characters with alphanumeric, lowercase, uppercase and/or special characters</p>
+        <p class="helper">We recommend you to chose a password of at least 12 characters with alphanumeric, lowercase, uppercase and/or special characters</p>
         <?= $this->Form->control('confirm_password', ['type' => 'password', 'required' => true]) ?>
     </fieldset>
     <?= $this->Form->submit(__('Submit new password')); ?>
