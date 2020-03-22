b18e99218fac729ecf392ad6523f8511a39f2e09
VigiePirate <artefact@vigies-pirates.net>
2020-03-22 13:35:00 +0100

    Syntax error

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -199,12 +199,11 @@ class UsersController extends AppController
                     $passkey = uniqid('', true);
                     $url = Router::Url(['controller' => 'users', 'action' => 'resetPassword'], true) . '/' . $passkey;
                     /* $timeout = time() + DAY; */
-                    $timeout = Chronos::now();
                     if ($this->Users->updateAll(
                       ['passkey' => $passkey],
                       ['id' => $user->id],
                       ['failed_login_attempts' => $user->failed_login_attempts++],
-                      ['failed_login_last_date'] => $timeout],
+                      ['failed_login_last_date' => Chronos::now()],
                     )){
                       $this->sendResetEmail($url, $user);
                       return $this->redirect(['action' => 'login']);
