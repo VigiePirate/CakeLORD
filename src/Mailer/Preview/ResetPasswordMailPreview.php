342df8cdb84264225149ac1bb0f9ae9610e33d80
VigiePirate <artefact@vigies-pirates.net>
2020-03-23 19:06:55 +0100

    Mail preview debug

--- src/Mailer/Preview/ResetPasswordMailPreview.php
+++ src/Mailer/Preview/ResetPasswordMailPreview.php
@@ -8,12 +8,11 @@ class PasswordMailPreview extends MailPreview
 {
     public function sendResetEmail()
     {
-        $mailer = $this->getMailer('sendResetEmail');
+        $this->getMailer('sendResetEmail');
         // set any template variables receipients for the mailer.
         $this->loadModel("Users");
-                        $user = $this->Users->find()->first();
-                        return $this->getMailer("User")
-                            ->sendResetEmail($user)
-        return $mailer;
+        $user = $this->Users->find()->first();
+        return $this->getMailer("User")
+                    ->sendResetEmail($url,$user)
     }
 }
