2b4bb9275cfd884b7a90b2f4ffbb6dbb2711766b
VigiePirate <artefact@vigies-pirates.net>
2020-03-23 14:40:57 +0100

    Changed something...

--- src/Mailer/PasswordMailPreview.php
+++ src/Mailer/PasswordMailPreview.php
@@ -1,15 +1,20 @@
-// in src/Mailer/MailPreview/WelcomePreview.php
+// in src/Mailer/MailPreview/PasswordMailPreview.php
+<?php
+
 namespace App\Mailer\Preview;
 
 use DebugKit\Mailer\MailPreview;
 
-class WelcomePreview extends MailPreview
+class PasswordMailPreview extends MailPreview
 {
-    public function welcome()
+    public function sendResetEmail()
     {
-        $mailer = $this->getMailer('Welcome');
+        $mailer = $this->getMailer('sendResetEmail');
         // set any template variables receipients for the mailer.
-
+        $this->loadModel("Users");
+                        $user = $this->Users->find()->first();
+                        return $this->getMailer("User")
+                            ->sendResetEmail($user)
         return $mailer;
     }
 }
