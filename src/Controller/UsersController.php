6dcf10a64f649b8c7c23132373554ac9a29029cf
VigiePirate <artefact@vigies-pirates.net>
2020-03-23 14:17:45 +0100

    changed return

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -234,7 +234,7 @@ class UsersController extends AppController
             } else {
                 $this->Flash->error(__('Error sending email: ')); // . $email->smtpError);
             }
-            return $this->$mailer;
+            return $mailer;
         }
 
     public function resetPassword($passkey = null) {
