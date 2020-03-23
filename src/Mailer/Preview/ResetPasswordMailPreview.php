b3b9267ca65aa6a49df094b673b8a017ea6d7db3
VigiePirate <artefact@vigies-pirates.net>
2020-03-23 19:23:40 +0100

    Restore previous mailer preview

--- src/Mailer/Preview/ResetPasswordMailPreview.php
+++ src/Mailer/Preview/ResetPasswordMailPreview.php
@@ -9,14 +9,7 @@ class PasswordMailPreview extends MailPreview
     public function sendResetEmail()
     {
         $this->getMailer('sendResetEmail');
-          ->setTransport('default')
-          ->setFrom(['lord@example.com' => 'Livre des Origines du Rat Domestique'])
-          // ->setSender('lord@example.com', 'MyApp emailer') // fixme
-          ->setTo($user->email)
-          ->setSubject('Reset your Password')
-          ->setViewVars(['url' => $url, 'username' => $user->username])
-          ->viewBuilder()
-            ->setTemplate('reset-password');
+        // set any template variables receipients for the mailer.
         $this->loadModel("Users");
         $user = $this->Users->find()->first();
         $this->Html->link(['controller' => 'users', 'action' => 'resetPassword', $passkey]);
