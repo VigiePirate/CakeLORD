caeb121745b386b680bb7cbdd6d156709eb771f5
VigiePirate <artefact@vigies-pirates.net>
2020-03-24 10:47:48 +0100

    Placed call to send in test

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -212,8 +212,8 @@ class UsersController extends AppController
                     ) {
 
                     // $mailer = $this->getMailer('User');//->sendResetEmail($url,$user);
-                    $mailer = $this->getMailer('User')->send('sendResetEmail', [$url, $user]);
-                    if ($mailer) {
+                    // $mailer = $this->getMailer('User')->send('sendResetEmail', [$url, $user]);
+                    if ($this->getMailer('User')->send('sendResetEmail', [$url, $user])) {
                           $this->Flash->success(__('Check your email for your reset password link'));
                       } else {
                           $this->Flash->error(__('Error sending email: ')); // . $email->smtpError);
