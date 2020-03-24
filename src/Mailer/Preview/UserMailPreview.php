5975120a89a3d29560207575d6623448a1ea4fe5
VigiePirate <artefact@vigies-pirates.net>
2020-03-24 13:38:31 +0100

    Mail dummy preview

--- src/Mailer/Preview/UserMailPreview.php
+++ src/Mailer/Preview/UserMailPreview.php
@@ -7,12 +7,12 @@ use Cake\Routing\Router;
 
 class UserMailPreview extends MailPreview
 {
-    public function sendResetEmail($url,$user)
+    public function sendResetEmail()
     {
         $this->loadModel("Users");
-        //$user = $this->Users->find()->first();
+        $user = $this->Users->find()->first();
         //$url = $this->Html->link(['controller' => 'users', 'action' => 'resetPassword', $user->$passkey]);
-        //$url = Router::Url(['controller' => 'users', 'action' => 'resetPassword'], true) . '/' . $user->$passkey;
+        $url = Router::Url(['controller' => 'users', 'action' => 'resetPassword'], true) . '/' . 'somepasskey';
         return $this->getMailer("User")
                     ->sendResetEmail($url,$user);
     }
