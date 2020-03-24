af5e080adf7781679ae4e10dac53264bf5061f5d
VigiePirate <artefact@vigies-pirates.net>
2020-03-24 12:33:57 +0100

    Changed names for preview files

---
+++ src/Mailer/Preview/UserMailPreview.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Mailer\Preview;
+
+use DebugKit\Mailer\MailPreview;
+
+class resetPasswordMailPreview extends MailPreview
+{
+    public function sendResetEmail()
+    {
+        $this->loadModel("Users");
+        $user = $this->Users->find()->first();
+        $url = $this->Html->link(['controller' => 'users', 'action' => 'resetPassword', $passkey]);
+        return $this->getMailer("User")
+                    ->sendResetEmail($url,$user);
+    }
+}
