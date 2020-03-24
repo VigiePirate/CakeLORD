9d3af21ea180e2ab3ee0bbd1ea68d7181d268a11
VigiePirate <artefact@vigies-pirates.net>
2020-03-24 10:45:51 +0100

    Just as planned!

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -212,8 +212,7 @@ class UsersController extends AppController
                     ) {
 
                     // $mailer = $this->getMailer('User');//->sendResetEmail($url,$user);
-                    // $mailer = $this->getMailer('User')->send('sendResetEmail', [$url, $user]);
-                    $mailer = $this->getMailer('User')->send('sendResetEmail');
+                    $mailer = $this->getMailer('User')->send('sendResetEmail', [$url, $user]);
                     if ($mailer) {
                           $this->Flash->success(__('Check your email for your reset password link'));
                       } else {
