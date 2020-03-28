2303a538293cfbc7e0de4f31ce53215897c1479e
Wodewood #Tchuck#chuck! <wodewoodTchuckchuck+gitgit@brobdingnag.pw>
2020-03-28 17:00:13 +0000

    added cell skeletons

---
+++ src/View/Cell/MyRatsCell.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\View\Cell;
+
+use Cake\View\Cell;
+
+/**
+ * MyRats cell
+ */
+class MyRatsCell extends Cell
+{
+    /**
+     * List of valid options that can be passed into this
+     * cell's constructor.
+     *
+     * @var array
+     */
+    protected $_validCellOptions = [];
+
+    /**
+     * Initialization logic run at the end of object construction.
+     *
+     * @return void
+     */
+    public function initialize(): void
+    {
+    }
+
+    /**
+     * Default display method.
+     *
+     * @return void
+     */
+    public function display()
+    {
+    }
+}
