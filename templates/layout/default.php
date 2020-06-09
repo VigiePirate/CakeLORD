ea60d2864753503b774e8599a94f16825f1b0184
VigiePirate <artefact@vigies-pirates.net>
2020-06-09 17:37:28 +0200

    Amaranth is ugly on small letters

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -29,7 +29,6 @@ $cakeDescription = 'LORD';
 
     <link href="https://fonts.googleapis.com/css?family=Alegreya:400,400i,500,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Imprima:400" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Amaranth:400,400i,500,700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css">
 
     <?= $this->Html->css('milligram.min.css') ?>
