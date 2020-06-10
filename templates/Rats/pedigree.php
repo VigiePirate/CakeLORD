f6bdd8be74e22f7ab7c425ae5850c45532009bb4
VigiePirate <artefact@vigies-pirates.net>
2020-06-10 21:22:42 +0200

    Create familytree markup

--- templates/Rats/pedigree.php
+++ templates/Rats/pedigree.php
@@ -34,9 +34,9 @@
             <!-- d3.js  -->
             <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
             <?= $this->Html->script('familytree'); ?>
-            <div id="familytree">
-                
-            </div>
+            <familytree>
+
+            </familytree>
             <!-- end copy-paste example -->
         </div>
     </div>
