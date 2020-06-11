452ccc569e9e33eac73db1bd43b90b01f62a2e09
VigiePirate <artefact@vigies-pirates.net>
2020-06-11 18:34:33 +0200

    With just w and h

--- webroot/js/familytree.js
+++ webroot/js/familytree.js
@@ -31,7 +31,7 @@ function setup() {
   var svg = d3.select("#familytree").append("svg")
     //.attr('width', window.innerWidth)
     //.attr('height', window.innerHeight)
-    .attr("viewBox", "0 0 900 600" ) // .attr("viewBox", "0 0 " + w + " " + h )
+    .attr("viewBox", "0 0 " + w + " " + h ) //.attr("viewBox", "0 0 900 600" )
     .attr("preserveAspectRatio", "xMidYMid meet")
     .call(zoom)
     .append('g')
