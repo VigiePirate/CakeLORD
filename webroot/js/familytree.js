65d1c2ba044c283b9526913a01e0e195080e0056
VigiePirate <artefact@vigies-pirates.net>
2020-06-12 18:49:39 +0200

    Increase separate?

--- webroot/js/familytree.js
+++ webroot/js/familytree.js
@@ -1,6 +1,6 @@
 var boxWidth = 210, //150 -> 180
     boxHeight = 70, //40 / 60
-    nodeWidth = 100, //100 -> 130?
+    nodeWidth = 100, //100
     nodeHeight = 220, //200 -> 210
 
     // duration of transitions in ms
@@ -8,7 +8,7 @@ var boxWidth = 210, //150 -> 180
 
     // d3 multiplies the node size by this value
     // to calculate the distance between nodes
-    separation = 0.8; // .5 -> 0.8
+    separation = 1; // .5 -> 0.8
 
 /**
  * For the sake of the examples, I want the setup code to be at the top.
