56b6c82703cb9a05828210c0496986e9f6e39aab
VigiePirate <artefact@vigies-pirates.net>
2020-06-14 12:42:02 +0200

    Right variable name in template

--- templates/Rats/pedigree.php
+++ templates/Rats/pedigree.php
@@ -53,7 +53,7 @@
                 separation = 0.8,
                 // data filename
                 // file = "/js/" + <?= $rat->id ?> + ".json";
-                json = <?= $rat->json ?>;
+                json = <?= $json ?>;
             setup(); //setup(file)
             </script>
             <!-- end family tree -->
