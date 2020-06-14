121fbefd743c34a2418cd6d8cd13527d77d2ca1c
VigiePirate <artefact@vigies-pirates.net>
2020-06-14 18:34:40 +0200

    Date format and full primary cause

--- src/Controller/RatsController.php
+++ src/Controller/RatsController.php
@@ -366,7 +366,7 @@ class RatsController extends AppController
         $this->Authorization->skipAuthorization();
         $rat = $this->Rats->get($id, [
             'contain' => ['Ratteries', 'BirthLitters', 'BirthLitters.Ratteries',
-            'BirthLitters.Sire','BirthLitters.Dam','BirthLitters.Sire.BirthLitters.Ratteries','BirthLitters.Dam.BirthLitters.Ratteries',
+            'BirthLitters.Sire','BirthLitters.Dam','BirthLitters.Sire.BirthLitters.Ratteries','BirthLitters.Dam.BirthLitters.Ratteries','BirthLitters.Dam.DeathPrimaryCauses'
             'Colors', 'Eyecolors', 'Dilutions', 'Markings', 'Earsets', 'Coats', 'DeathPrimaryCauses', 'DeathSecondaryCauses', 'States',
             'BredLitters','BredLitters.Sire','BredLitters.Dam','BredLitters.OffspringRats','BredLitters.OffspringRats.OwnerUsers','BredLitters.OffspringRats.States','BredLitters.OffspringRats.DeathPrimaryCauses','BredLitters.OffspringRats.DeathSecondaryCauses',
             'Singularities'],
@@ -379,7 +379,7 @@ class RatsController extends AppController
             '0' => [
                 'name' => $rat->birth_litter->dam[0]->usual_name,
                 'sex' => 'F',
-                'description' => $rat->birth_litter->dam[0]->birth_date . $rat->birth_litter->dam[0]->death_date, //should be $dam->variety
+                'description' => $rat->birth_litter->dam[0]->birth_datei18nFormat('dd/MM/yyyy') . '-' . $rat->birth_litter->dam[0]->death_datei18nFormat('dd/MM/yyyy'), //should be $dam->variety
                 'death'=> $rat->birth_litter->dam[0]->primary_death_cause->name, //should be short_death_cause
                 'id' => $rat->birth_litter->dam[0]->pedigree_identifier, // should be modified to be unique in the tree
                 '_parents' => [] // will call dam's parents in recursive implementation
