aacd0b9b3797b78f5d054ecf9744336274a58907
VigiePirate <artefact@vigies-pirates.net>
2020-06-15 18:02:02 +0200

    Elevate text

--- webroot/js/familytree.js
+++ webroot/js/familytree.js
@@ -496,7 +496,7 @@ function sexStroke(d){
         line.pop();
         tspan.text(line.join(" "));
         line = [word];
-        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + 1 + "em").text(word);
       }
     }
   });
