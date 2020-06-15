925f56e4808ac547ab278e75fdb9f9ab99ae882e
VigiePirate <artefact@vigies-pirates.net>
2020-06-15 17:22:37 +0200

    Wrap long names

--- webroot/js/familytree.js
+++ webroot/js/familytree.js
@@ -313,7 +313,8 @@ Tree.prototype.drawNodes = function(nodes, source){
   nodeUpdate.select('text.name')
       .attr("dx", -(boxWidth/2) + 8)
       .attr("dy", -19) //.attr("dy", -13)
-      .style('fill-opacity', 1);
+      .style('fill-opacity', 1)
+      .call(wrap, boxWidth);
 
   nodeUpdate.select('text.dates')
       .attr("dx", -(boxWidth/2) + 8)
@@ -465,4 +466,32 @@ function sexStroke(d){
       return "#56d88d";
     }
   }
+}
+
+/**
+ * Mike Bostock wrap text function
+ */
+
+ function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
 }
