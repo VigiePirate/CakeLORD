f1ea803f9684822b5dcd79b379fca1c3deba703d
VigiePirate <artefact@vigies-pirates.net>
2020-06-15 19:10:24 +0200

    Syntax

--- webroot/js/familytree.js
+++ webroot/js/familytree.js
@@ -314,7 +314,7 @@ Tree.prototype.drawNodes = function(nodes, source){
       .call(wrap, (boxWidth-20)) // 8u margin on left and right + border thickness
       //.attr("dx", -(boxWidth/2) + 8)
       // .attr("dy", -50) //.attr("dy", -13),
-      .attr("text-anchor", "middle"),
+      .attr("text-anchor", "middle")
       .style('fill-opacity', 1);
 
 
