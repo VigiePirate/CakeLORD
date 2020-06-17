e3321105e1ba8657c23f9fdf48376c79cf513540
VigiePirate <artefact@vigies-pirates.net>
2020-06-17 15:06:45 +0200

    Test with separation modulation

--- webroot/js/familytree.js
+++ webroot/js/familytree.js
@@ -122,8 +122,12 @@ var Tree = function(svg, selector, direction){
       // By default, cousins are drawn further apart than siblings.
       // By returning the same value in all cases, we draw cousins
       // the same distance apart as siblings -> do we really want that?
-      .separation(function(){
-        return separation;
+      // .separation(function(){
+      //  return separation;
+      // });
+
+      .separation(function(a,b){
+        return  a.parent === b.parent ? separation : (separation*4/3);
       });
 };
 
