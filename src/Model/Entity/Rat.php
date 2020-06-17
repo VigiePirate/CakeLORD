1e037fbbca3d8ffefbca36ee151ca341cc226fd0
VigiePirate <artefact@vigies-pirates.net>
2020-06-17 18:35:50 +0200

    Fix syntax error in double prefix getter

--- src/Model/Entity/Rat.php
+++ src/Model/Entity/Rat.php
@@ -123,7 +123,7 @@ class Rat extends Entity
     {
         $doublePrefix = $this->rattery->prefix;
 
-        if(isset($this->birth_litter) && !empty($birth_litter->contributions[1]) {
+        if(isset($this->birth_litter) && !empty($birth_litter->contributions[1])) {
             if(!empty($birth_litter->contributions[1]) {
                 $doublePrefix .= '-' . $birth_litter->contributions[1]->rattery->prefix;
             }
