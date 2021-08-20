15e47a77970c742cb69fe4a2c9de8b7685baf707
VigiePirate <artefact@vigies-pirates.net>
2021-08-20 19:51:35 +0200

    Colors

--- templates/Rats/my.php
+++ templates/Rats/my.php
@@ -11,9 +11,9 @@
             <input class="radio" id="two" name="group" type="radio">
             <input class="radio" id="three" name="group" type="radio">
             <div class="tabs">
-                <label class="tab" id="one-tab" for="one"> <?= __('All my rats') ?> </label>
+                <label class="tab" id="one-tab" for="one"> <?= __('My rats') ?> </label>
                 <label class="tab" id="two-tab" for="two"> <?= __('My females') ?> </label>
-                <label class="tab" id="three-tab" for="three"> <?= __('My rats') ?> </label>
+                <label class="tab" id="three-tab" for="three"> <?= __('My males') ?> </label>
             </div>
             <div class="panels">
                 <div class="panel users content view" id="one-panel">
