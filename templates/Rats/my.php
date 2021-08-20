e70eddeb0ad2ebd73ea15f989f51bb25f7b24b78
VigiePirate <artefact@vigies-pirates.net>
2021-08-20 19:28:19 +0200

    Div mandatory in panel ?

--- templates/Rats/my.php
+++ templates/Rats/my.php
@@ -8,36 +8,6 @@
     <div class="column-responsive column-90">
 
         <div class="tab-wrapper">
-          <input class="radio" id="one" name="group" type="radio" checked>
-          <input class="radio" id="two" name="group" type="radio">
-          <input class="radio" id="three" name="group" type="radio">
-          <div class="tabs">
-          <label class="tab" id="one-tab" for="one">CSS</label>
-          <label class="tab" id="two-tab" for="two">Skills</label>
-          <label class="tab" id="three-tab" for="three">Prerequisites</label>
-            </div>
-          <div class="panels">
-          <div class="panel" id="one-panel">
-            <div class="panel-title">Why Learn CSS?</div>
-            <p>Without CSS, every web page would be drab plain text and images that flowed straight down the page. With CSS, you can add color and background images and change the layout of your page — your web pages can feel like works of art!</p>
-          </div>
-          <div class="panel" id="two-panel">
-            <div class="panel-title">Take-Away Skills</div>
-            <p>You will learn many aspects of styling web pages! You’ll be able to set up the correct file structure, edit text and colors, and create attractive layouts. With these skills, you’ll be able to customize the appearance of your web pages to suit your every need!</p>
-          </div>
-          <div class="panel" id="three-panel">
-            <div class="panel-title">Note on Prerequisites</div>
-            <p>We recommend that you complete Learn HTML before learning CSS.</p>
-          </div>
-          </div>
-        </div>
-
-
-<div class="spacer"> </div>
-<div class="spacer"> </div>
-<div class="spacer"> </div>
-
-        <div class="tab-wrapper">
             <input class="radio" id="one" name="group" type="radio" checked>
             <input class="radio" id="two" name="group" type="radio">
             <input class="radio" id="three" name="group" type="radio">
@@ -49,13 +19,13 @@
         </div>
         <div class="panels">
             <div class="panel" id="one-panel">
-                <p>Panel 1</p>
+                <div>Panel 1</div>
             </div>
             <div class="panel" id="two-panel">
-                <p>Panel 2</p>
+                <div>Panel 2</div>
             </div>
             <div class="panel" id="three-panel">
-                <p>Panel 3</p>
+                <div>Panel 3</div>
             </div>
         </div>
 
