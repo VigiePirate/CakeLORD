f3d49e6eded7a90613ea6fd803ecbd33f88f53fa
VigiePirate <artefact@vigies-pirates.net>
2021-08-20 18:00:52 +0200

    Test tabs

--- templates/Rats/my.php
+++ templates/Rats/my.php
@@ -4,6 +4,39 @@
             <?= $this->element('my/sidebar') ?>
         </div>
     </aside>
+
+    <div class="mg-tabs" data-toggle="tabs" id="tabs-example">
+                  <ul>
+                    <li class="mg-tabs--item active" data-target="tab1">
+                      <a href="#" class="secondary">Tab one</a>
+                    </li>
+                    <li class="mg-tabs--item" data-target="tab2">
+                      <a href="#" class="button--outline">Tab two</a>
+                    </li>
+                    <li class="mg-tabs--item" data-target="tab3">
+                      <a href="#" class="secondary">Tab one</a>
+                    </li>
+                  </ul>
+                  <div class="mg-tabs--content" id="tab1">
+                    1 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Magni iste, placeat voluptates sapiente blanditiis fuga ullam.
+                    Iure dolorum libero fugit quidem, voluptate veniam aut animi
+                    nihil voluptas mollitia, aliquid inventore.
+                  </div>
+                  <div class="mg-tabs--content" id="tab2">
+                    2 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Magni iste, placeat voluptates sapiente blanditiis fuga ullam.
+                    Iure dolorum libero fugit quidem, voluptate veniam aut animi
+                    nihil voluptas mollitia, aliquid inventore.
+                  </div>
+                  <div class="mg-tabs--content" id="tab3">
+                    3 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Magni iste, placeat voluptates sapiente blanditiis fuga ullam.
+                    Iure dolorum libero fugit quidem, voluptate veniam aut animi
+                    nihil voluptas mollitia, aliquid inventore.
+                  </div>
+                </div>
+
     <div class="column-responsive column-90">
         <div class="users view content">
                 <?= $this->element('rats', [
--- templates/Users/my.php
+++ templates/Users/my.php
@@ -9,18 +9,6 @@
 
     <div class="column-responsive column-90">
         <div class="users view content">
-
-
-            <div class="mg-nav mg-nav--inline" id="nav-horizontal-example" data-toggle="nav">
-                <ul>
-                    <li class="active"><a href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li class="mg-right"><a href="#about">About</a></li>
-                </ul>
-            </div>
-
-
             <?= $this->element('card', [
                 'image' => $user->avatar,
                 'rubric' => $user->username,
