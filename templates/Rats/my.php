c6b8dc1696992784add0f1d87856a2461a5e9ed0
VigiePirate <artefact@vigies-pirates.net>
2021-08-21 21:24:21 +0200

    Tab icon css width, labels

--- templates/Rats/my.php
+++ templates/Rats/my.php
@@ -11,36 +11,34 @@
             <input class="radio" id="two" name="group" type="radio">
             <input class="radio" id="three" name="group" type="radio">
             <input class="radio" id="four" name="group" type="radio">
+            <input class="radio" id="five" name="group" type="radio">
+            <input class="radio" id="six" name="group" type="radio">
             <div class="tabs">
                 <label class="tab" id="one-tab" for="one">
                     <?= $this->Html->image('/img/icon-white-rat.svg', [
                         'class' => 'tab-icon',
-                        'alt' => __('My rats'),
-                        "width" => "36"]) ?>
+                        'alt' => __('My colony')]) ?>
                 </label>
                 <label class="tab" id="two-tab" for="two">
                     <?= $this->Html->image('/img/icon-white-rat.svg', [
                         'class' => 'tab-icon',
-                        'alt' => __('My females'),
-                        "width" => "36"]) ?>
+                        'alt' => __('My females')]) ?>
                 </label>
                 <label class="tab" id="three-tab" for="three">
                     <?= $this->Html->image('/img/icon-white-rat.svg', [
                         'class' => 'tab-icon',
-                        'alt' => __('My males'),
-                        "width" => "36"]) ?>
+                        'alt' => __('My males')]) ?>
                 </label>
                 <label class="tab" id="four-tab" for="four">
                     <?= $this->Html->image('/img/icon-white-rat.svg', [
                         'class' => 'tab-icon',
-                        'alt' => __('My lost'),
-                        "width" => "36"]) ?>
+                        'alt' => __('My rainbow brigde')]) ?>
                 </label>
             </div>
             <div class="panels">
                 <div class="panel users content view" id="one-panel">
                     <?= $this->element('rats', [
-                        'rubric' => __('All my rats'),
+                        'rubric' => __('My colony'),
                         'exceptions' => [
                             'pup_name',
                             'birth_date',
