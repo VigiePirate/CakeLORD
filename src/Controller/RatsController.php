091b11c7048faad05e528ccd8489bc4957fda1d0
VigiePirate <artefact@vigies-pirates.net>
2021-08-22 20:49:41 +0200

    Syntax

--- src/Controller/RatsController.php
+++ src/Controller/RatsController.php
@@ -55,7 +55,7 @@ class RatsController extends AppController
             'contain' => ['Ratteries','OwnerUsers', 'States', 'DeathPrimaryCauses', 'DeathSecondaryCauses','BirthLitters','BirthLitters.Contributions','BirthLitters.Ratteries'],
         ];
         $rats = $this->paginate($this->Rats->find()->where(['Rats.owner_user_id' => $user->id]));
-        $females = $this->paginate($this->Rats->find()->where(['Rats.owner_user_id' => $user->id, 'Rats.sex' => 'F']);
+        $females = $this->paginate($this->Rats->find()->where(['Rats.owner_user_id' => $user->id, 'Rats.sex' => 'F']));
         $this->set(compact('females','rats', 'user'));
     }
 
