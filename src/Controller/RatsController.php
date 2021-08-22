2c416f75988ab14d8ddd510d8b711381063ae729
VigiePirate <artefact@vigies-pirates.net>
2021-08-22 20:48:56 +0200

    New query

--- src/Controller/RatsController.php
+++ src/Controller/RatsController.php
@@ -54,8 +54,8 @@ class RatsController extends AppController
         $this->paginate = [
             'contain' => ['Ratteries','OwnerUsers', 'States', 'DeathPrimaryCauses', 'DeathSecondaryCauses','BirthLitters','BirthLitters.Contributions','BirthLitters.Ratteries'],
         ];
-        $rats = $this->Rats->find()->where(['Rats.owner_user_id' => $user->id]);
-        $females = $rats->find()->where(['Rats.sex' => 'F']);
+        $rats = $this->paginate($this->Rats->find()->where(['Rats.owner_user_id' => $user->id]));
+        $females = $this->paginate($this->Rats->find()->where(['Rats.owner_user_id' => $user->id, 'Rats.sex' => 'F']);
         $this->set(compact('females','rats', 'user'));
     }
 
