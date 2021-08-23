9110a5a64bcb9cc4b1d35fdff189e9722b88cd98
VigiePirate <artefact@vigies-pirates.net>
2021-08-23 15:40:31 +0200

    My litters

--- templates/Litters/my.php
+++ templates/Litters/my.php
@@ -5,16 +5,23 @@
         </div>
     </aside>
     <div class="column-responsive column-90">
-        <div class="users view content">
-                <?= $this->element('litters', [
-                    'rubric' => __('My Litters'),
-                    'litters' => $litters,
-                    'exceptions' => [
-                        'mating_date',
-                        'full_name',
-                        'pups_number_stillborn',
-                    ],
-                ]) ?>
+        <div class="litters view content">
+            <?= $this->Html->link(__('New Litter'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+            <div class="sheet-heading">
+                <div class="sheet-title pretitle"><?= h($user->username) . '’s dashboard' ?></div>
+            </div>
+            <h1><?= __('My litters') ?> </h1>
+            <h2><?= __('Summary')</h2>
+            <h2><?= __('Statistics')</h2>
+            <?= $this->element('litters', [
+                'rubric' => __('All my Litters'),
+                'litters' => $litters,
+                'exceptions' => [
+                    'mating_date',
+                    'full_name',
+                    'pups_number_stillborn',
+                ],
+            ]) ?>
         </div>
     </div>
 </div>
