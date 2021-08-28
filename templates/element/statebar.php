24b102b3495519cc7c04ace90f8dd24a7b03b95e
VigiePirate <artefact@vigies-pirates.net>
2021-08-28 14:41:32 +0200

    Add some arrows

--- templates/element/statebar.php
+++ templates/element/statebar.php
@@ -5,7 +5,7 @@
 
     <!-- else, if user is a staff member, show full state bar -->
     <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
-    <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
+    <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"> << <?= h($rat->state->symbol) ?> >> </div>
     <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
 
 </div>
