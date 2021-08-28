6f71498618dd3777385da7685ed583149e289a45
VigiePirate <artefact@vigies-pirates.net>
2021-08-28 14:17:19 +0200

    Add next ko state

--- templates/element/statebar.php
+++ templates/element/statebar.php
@@ -1,6 +1,7 @@
-
 <div class="sheet-markers">
     <!-- if guest or non-staff user, show only current state -->
+
+    <!-- else, if user is a staff member, show full state bar -->
+    <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($rat->state->next_ko_state->symbol) ?></div>
     <div class="statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
-    <!-- else, if user is a staff member, show full state bar --> 
 </div>
