9aaf3888725655c4c496d0de265bcb28abc5b7c3
VigiePirate <artefact@vigies-pirates.net>
2021-08-28 14:40:00 +0200

    Comment about css property

--- templates/element/statebar.php
+++ templates/element/statebar.php
@@ -1,9 +1,11 @@
+<!-- ugly style to be replaced later by state->css_property -->
+
 <div class="sheet-markers">
     <!-- if guest or non-staff user, show only current state -->
 
     <!-- else, if user is a staff member, show full state bar -->
     <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
     <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
-    <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
+    <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
 
 </div>
