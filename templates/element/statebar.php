446c1fb5420063c045e72d2b39f4d0fe739766e7
VigiePirate <artefact@vigies-pirates.net>
2021-08-29 17:57:34 +0200

    Process user action separately

--- templates/element/statebar.php
+++ templates/element/statebar.php
@@ -8,14 +8,18 @@
     <!-- if state is not frozen -->
     <?php if( !$rat->state->is_frozen ) : ?>
         <!-- if state needs staff action: show newt frozen, next ko, next ok -->
-        <?php if( $rat->state->needs_staff_action || $rat->state->needs_user_action) : ?>
+        <?php if( $rat->state->needs_staff_action : ?>
             <div class="statemark statecolor_<?php echo h($rat->state->next_frozen_state_id) ?>"><?= h($next_frozen_state->symbol) ?></div>
             <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
             <div class="staff-action-symbol">⮜</div>
             <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
             <div class="staff-action-symbol">⮞</div>
             <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
-        <!-- if state doesn't any action: show next ko, next frozen -->
+        <!-- if state needs staff action: the only option is to bring it back to backoffice-->
+        <?php else if( $rat->state->needs_user_action) : ?>
+            <div class="staff-action-symbol">⮜</div>
+            <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
+        <!-- if state doesn't any action: show next ko, next frozen -->                
         <?php else : ?>
             <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
             <div class="staff-action-symbol">⮜</div>
