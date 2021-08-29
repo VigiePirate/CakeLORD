894819a5063e34af01ad32489f4e929163b64ea5
VigiePirate <artefact@vigies-pirates.net>
2021-08-29 18:44:30 +0200

    Restore all elses

--- templates/element/statebar.php
+++ templates/element/statebar.php
@@ -8,7 +8,6 @@
 
     <!-- if state is not frozen -->
     <?php if( !$rat->state->is_frozen ) : ?>
-
         <!-- if state needs staff action: show newt frozen, next ko, next ok -->
         <?php if( $rat->state->needs_staff_action ) : ?>
             <?php if( !empty($rat->state->next_frozen_state_id) ) : ?>
@@ -19,25 +18,19 @@
             <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
             <div class="staff-action-symbol">⮞</div>
             <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
-        <?php endif; ?>
-
         <!-- if state needs user action: nothing to do-->
-        <?php if( $rat->state->needs_user_action ) : ?>
+        <?php elseif( $rat->state->needs_user_action ) : ?>
             <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
-        <?php endif; ?>
-
-        <!-- if state doesn't any action: show next ko, next frozen -->
-        <?php if( !$rat->state->needs_user_action && !$rat->state->needs_user_action ) : ?>
+        <?php else : ?>
+            <!-- if state doesn't any action: show next ko, next frozen -->
             <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
             <div class="staff-action-symbol">⮜</div>
             <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
             <div class="staff-action-symbol">⮞</div>
             <div class="statemark statecolor_<?php echo h($rat->state->next_frozen_state_id) ?>"><?= h($next_frozen_state->symbol) ?></div>
-
         <?php endif; ?>
-    <?php endif; ?>
-    <!-- if state is frozen, show next thawed in the right order (depending on reliability) -->
-    <?php if( $rat->state->is_frozen ) : ?>
+    <?php else : ?>
+        <!-- if state is frozen, show next thawed in the right order (depending on reliability) -->
         <?php if( $rat->state->is_reliable ) : ?>
             <div class="statemark statecolor_<?php echo h($rat->state->next_thawed_id) ?>"><?= h($next_thawed_state->symbol) ?></div>
             <div class="staff-action-symbol">⮜</div>
