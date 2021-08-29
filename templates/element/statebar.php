740d45cce3cb2bad09da3c1475d0bf98fd44b47a
VigiePirate <artefact@vigies-pirates.net>
2021-08-29 14:12:26 +0200

    show same everywhere for now

--- templates/element/statebar.php
+++ templates/element/statebar.php
@@ -6,28 +6,29 @@
     <!-- else, if user is a staff member, show full state bar -->
 
     <!-- if state is not frozen -->
-    <?php if() : ?>
-    <!-- if state doesn't need staff action: show next ko, next frozen -->
-        <?php if() : ?>
+    <?php if( !$rat->state->is_frozen ) : ?>
         <!-- if state needs staff action: show newt frozen, next ko, next ok -->
+        <?php if( !$rat->state->needs_staff_action) : ?>
+            <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
+            <div class="staff-action-symbol">《</div>
+            <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
+            <div class="staff-action-symbol">》</div>
+            <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
+        <!-- if state doesn't need staff action: show next ko, next frozen -->
         <?php else : ?>
-
+            <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
+            <div class="staff-action-symbol">《</div>
+            <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
+            <div class="staff-action-symbol">》</div>
+            <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
         <?php endif; ?>
 
     <!-- if state is frozen, show next thawed in the right order (using is reliable) -->
     <?php else : ?>
-
-
+        <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
+        <div class="staff-action-symbol">《</div>
+        <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
+        <div class="staff-action-symbol">》</div>
+        <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
     <?php endif; ?>
-
-
-
-
-
-    <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
-    <div class="staff-action-symbol">《</div>
-    <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
-    <div class="staff-action-symbol">》</div>
-    <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
-
 </div>
