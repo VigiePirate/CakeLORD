a7b4c1372e1028a426f8c2c9fe6fb5299dce67c7
VigiePirate <artefact@vigies-pirates.net>
2021-08-29 18:00:39 +0200

    When state needs user action, staff don't do anything

--- templates/element/statebar.php
+++ templates/element/statebar.php
@@ -17,9 +17,8 @@
             <div class="statemark statecolor_<?php echo h($rat->state->next_ok_state_id) ?>"><?= h($next_ok_state->symbol) ?></div>
         <!-- if state needs staff action: the only option is to bring it back to backoffice-->
         <?php else if( $rat->state->needs_user_action) : ?>
-            <div class="staff-action-symbol">⮜</div>
             <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
-        <!-- if state doesn't any action: show next ko, next frozen -->                
+        <!-- if state doesn't any action: show next ko, next frozen -->
         <?php else : ?>
             <div class="statemark statecolor_<?php echo h($rat->state->next_ko_state_id) ?>"><?= h($next_ko_state->symbol) ?></div>
             <div class="staff-action-symbol">⮜</div>
