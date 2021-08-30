d109beff436b947160cd9795e589a4e2021e196d
VigiePirate <artefact@vigies-pirates.net>
2021-08-30 12:30:45 +0200

    Syntax

--- templates/element/my/user-summary.php
+++ templates/element/my/user-summary.php
@@ -1,6 +1,6 @@
 <div class="float-right">
     <?= $this->Html->image($user->avatar, ['alt' => $user->username, 'width' => '20%']) ?>
 </div>
-<h1><?= __('Welcome!')</h1>
+<h1><?= __('Welcome!') ?></h1>
 
 <p><?= h($user->email) ?> (<?= $user->has('role') ? $this->Html->link($user->role->name, ['controller' => 'Roles', 'action' => 'view', $user->role->id]) : '' ?>)</p>
