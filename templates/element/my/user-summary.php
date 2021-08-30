e32ea692200e781571236d86d4e1fb5c1dfd9d99
VigiePirate <artefact@vigies-pirates.net>
2021-08-30 12:29:29 +0200

    Wording in dashboard

--- templates/element/my/user-summary.php
+++ templates/element/my/user-summary.php
@@ -1,8 +1,6 @@
 <div class="float-right">
     <?= $this->Html->image($user->avatar, ['alt' => $user->username, 'width' => '20%']) ?>
 </div>
-<h1><?= h($user->username) ?></h1>
-
-<p><?= __('Welcome on your dashboard!')</p>
+<h1><?= __('Welcome!')</h1>
 
 <p><?= h($user->email) ?> (<?= $user->has('role') ? $this->Html->link($user->role->name, ['controller' => 'Roles', 'action' => 'view', $user->role->id]) : '' ?>)</p>
