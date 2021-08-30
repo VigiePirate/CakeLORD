a0fb7aad7eab3c8cf0c0bf64a3519ef07c4a61ec
VigiePirate <artefact@vigies-pirates.net>
2021-08-30 13:01:03 +0200

    Width problem

--- templates/element/my/user-summary.php
+++ templates/element/my/user-summary.php
@@ -1,4 +1,6 @@
+<div class="float-right">
+    <?= $this->Html->image($user->avatar, ['alt' => $user->username, 'width' => '20%']) ?>
+</div>
 
 <h1><?= __('Welcome!') ?></h1>
-
 Write something here? (Last/failed connection time?)
