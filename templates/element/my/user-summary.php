f6774558217cef6c52e382f25432772513e2e04d
VigiePirate <artefact@vigies-pirates.net>
2021-08-30 12:26:20 +0200

    dashboard text

--- templates/element/my/user-summary.php
+++ templates/element/my/user-summary.php
@@ -2,4 +2,7 @@
     <?= $this->Html->image($user->avatar, ['alt' => $user->username, 'width' => '20%']) ?>
 </div>
 <h1><?= h($user->username) ?></h1>
+
+<p><?= __('Welcome on your dashboard!')</p>
+
 <p><?= h($user->email) ?> (<?= $user->has('role') ? $this->Html->link($user->role->name, ['controller' => 'Roles', 'action' => 'view', $user->role->id]) : '' ?>)</p>
