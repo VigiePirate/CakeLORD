ab6146e72fcac35b748b8ae3c15450a969fb11b0
VigiePirate <artefact@vigies-pirates.net>
2021-08-30 12:59:00 +0200

    Move avatar above

--- templates/element/my/user-summary.php
+++ templates/element/my/user-summary.php
@@ -1,6 +1,4 @@
-<div class="float-right">
-    <?= $this->Html->image($user->avatar, ['alt' => $user->username, 'width' => '20%']) ?>
-</div>
+
 <h1><?= __('Welcome!') ?></h1>
 
 Write something here? (Last/failed connection time?)
