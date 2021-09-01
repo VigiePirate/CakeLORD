fb5cd92b2712df3be8165c53177a6ac5b7d6f879
VigiePirate <artefact@vigies-pirates.net>
2021-09-01 15:47:30 +0200

    Align staff actions to right

--- templates/element/staff_sidebar.php
+++ templates/element/staff_sidebar.php
@@ -1,15 +1,17 @@
 <div class="spacer"> </div>
-<div class="tooltip-staff">
-    <?= $this->Html->image('/img/icon-edit-as-staff.svg', [
-        'url' => ['controller' => 'Rats', 'action' => 'edit', $rat->id],
-        'class' => 'side-nav-icon',
-        'alt' => __('Edit Rat as Admin')]) ?>
-    <span class="tooltiptext-staff"><?= __('Edit rat data as staff') ?></span>
-</div>
+<div class="actions">
+    <div class="tooltip-staff">
+        <?= $this->Html->image('/img/icon-edit-as-staff.svg', [
+            'url' => ['controller' => 'Rats', 'action' => 'edit', $rat->id],
+            'class' => 'side-nav-icon',
+            'alt' => __('Edit Rat as Admin')]) ?>
+        <span class="tooltiptext-staff"><?= __('Edit rat data as staff') ?></span>
+    </div>
 
-<div class="tooltip-staff">
-    <?= $this->Html->image('/img/icon-delete.svg', [
-        'class' => 'side-nav-icon',
-        'alt' => __('Delete Rat')]) ?>
-    <span class="tooltiptext-staff"><?= __('Delete rat') ?></span>
+    <div class="tooltip-staff">
+        <?= $this->Html->image('/img/icon-delete.svg', [
+            'class' => 'side-nav-icon',
+            'alt' => __('Delete Rat')]) ?>
+        <span class="tooltiptext-staff"><?= __('Delete rat') ?></span>
+    </div>
 </div>
