d3bf133d6a8f51f3c9c223092e06646b65c4305a
VigiePirate <artefact@vigies-pirates.net>
2021-09-01 17:18:46 +0200

    Remove spans

--- templates/Ratteries/view.php
+++ templates/Ratteries/view.php
@@ -17,7 +17,6 @@
                     'url' => ['controller' => 'Ratteries', 'action' => 'locate', $rattery->id],
                     'class' => 'side-nav-icon',
                     'alt' => __('See on Map')]) ?>
-                    <span class="side-nav-text hide-everywhere"><?= __('See on map') ?><span>
                 <!-- </div>
 
                 <!-- <div class="side-nav-item"> -->
@@ -25,7 +24,6 @@
                     'url' => ['controller' => 'Ratteries', 'action' => 'edit', $rattery->id],
                     'class' => 'side-nav-icon',
                     'alt' => __('Modify Rattery')]) ?>
-                    <span class="side-nav-text hide-everywhere"><?= __('Edit') ?><span>
                 <!-- </div> -->
 
                 <!-- <div class="side-nav-item"> -->
@@ -33,7 +31,6 @@
                     'url' => ['controller' => 'Litters', 'action' => 'add'], //pass rattery id as contributor ? $rattery->id],
                     'class' => 'side-nav-icon',
                     'alt' => __('Declare Litter')]) ?>
-                <span class="side-nav-text hide-everywhere"><?= __('Add litter') ?><span>
                 <!-- </div> -->
             </div>
 
