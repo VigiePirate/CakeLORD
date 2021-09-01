f35cba135235eebe36280d4f2a343dbcdb094d6b
VigiePirate <artefact@vigies-pirates.net>
2021-09-01 15:48:50 +0200

    Align right was a bad idea

--- templates/element/staff_sidebar.php
+++ templates/element/staff_sidebar.php
@@ -1,17 +1,16 @@
 <div class="spacer"> </div>
-<div class="actions">
-    <div class="tooltip-staff">
-        <?= $this->Html->image('/img/icon-edit-as-staff.svg', [
-            'url' => ['controller' => 'Rats', 'action' => 'edit', $rat->id],
-            'class' => 'side-nav-icon',
-            'alt' => __('Edit Rat as Admin')]) ?>
-        <span class="tooltiptext-staff"><?= __('Edit rat data as staff') ?></span>
-    </div>
 
-    <div class="tooltip-staff">
-        <?= $this->Html->image('/img/icon-delete.svg', [
-            'class' => 'side-nav-icon',
-            'alt' => __('Delete Rat')]) ?>
-        <span class="tooltiptext-staff"><?= __('Delete rat') ?></span>
-    </div>
+<div class="tooltip-staff">
+    <?= $this->Html->image('/img/icon-edit-as-staff.svg', [
+        'url' => ['controller' => 'Rats', 'action' => 'edit', $rat->id],
+        'class' => 'side-nav-icon',
+        'alt' => __('Edit Rat as Admin')]) ?>
+    <span class="tooltiptext-staff"><?= __('Edit rat data as staff') ?></span>
+</div>
+
+<div class="tooltip-staff">
+    <?= $this->Html->image('/img/icon-delete.svg', [
+        'class' => 'side-nav-icon',
+        'alt' => __('Delete Rat')]) ?>
+    <span class="tooltiptext-staff"><?= __('Delete rat') ?></span>
 </div>
