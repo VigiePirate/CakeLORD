b16bb8a2c801ab7c02628d6cb741e151ea9662c7
VigiePirate <artefact@vigies-pirates.net>
2021-09-04 13:58:57 +0200

    List icon in articles

--- templates/Articles/view.php
+++ templates/Articles/view.php
@@ -11,6 +11,15 @@
                 <?= $this->element('default_sidebar') ?>
             </div>
             <div class="side-nav-group">
+                <div class="tooltip">
+                    <?= $this->Html->image('/img/icon-list.svg', [
+                        'url' => ['controller' => 'Articles', 'action' => 'index'],
+                        'class' => 'side-nav-icon',
+                        'alt' => __('All articles')]) ?>
+                    <span class="tooltiptext"><?= __('See all articles') ?></span>
+                </div>
+            </div>
+            <div class="side-nav-group">
                 <?= $this->element('staff_sidebar', [
                     'controller' => 'Articles',
                     'object' => $article
