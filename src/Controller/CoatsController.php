a4e47c31ee2b10b5fef09952389d308f4bc7819d
VigiePirate <artefact@vigies-pirates.net>
2021-09-05 16:46:32 +0200

    Order rand

--- src/Controller/CoatsController.php
+++ src/Controller/CoatsController.php
@@ -34,7 +34,9 @@ class CoatsController extends AppController
     public function view($id = null)
     {
         $coat = $this->Coats->get($id);
-        $examples = $this->Coats->Rats->find('list', [['order' => rand()]]);
+        $examples = $this->Coats->Rats->find('list', [
+            ['order' => 'rand()'],
+            ['limit' => '5']]);
         $this->set(compact('coat','examples'));
     }
 
