e904abde27351c576513990d4b63ab088129472c
VigiePirate <artefact@vigies-pirates.net>
2021-09-05 19:37:45 +0200

    Edit menu for articles

--- templates/Articles/edit.php
+++ templates/Articles/edit.php
@@ -7,13 +7,35 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <?= $this->element('default_sidebar') ?>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $article->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $article->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Articles'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <div class="side-nav-group">
+                <?= $this->element('default_sidebar') ?>
+            </div>
+
+            <div class="side-nav-group">
+                <div class="tooltip">
+                    <?= $this->Html->image('/img/icon-back.svg', [
+                        'url' => ['controller' => 'Articles', 'action' => 'view',$article->id],
+                        'class' => 'side-nav-icon',
+                        'alt' => __('Back')]) ?>
+                    <span class="tooltiptext"><?= __('Cancel and go back') ?></span>
+                </div>
+                <div class="tooltip">
+                    <?= $this->Html->image('/img/icon-list.svg', [
+                        'url' => ['controller' => 'Articles', 'action' => 'index'],
+                        'class' => 'side-nav-icon',
+                        'alt' => __('All articles')]) ?>
+                    <span class="tooltiptext"><?= __('See all articles') ?></span>
+                </div>
+            </div>
+
+            <div class="side-nav-group">
+                <div class="tooltip-staff">
+                    <?= $this->Html->image('/img/icon-delete.svg', [
+                        'class' => 'side-nav-icon',
+                        'alt' => __('Delete Rattery')]) ?>
+                    <span class="tooltiptext-staff"><?= __('Delete rattery') ?></span>
+                </div>
+            </div>
         </div>
     </aside>
     <div class="column-responsive column-90">
