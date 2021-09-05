f0c0f054230cc2cb35259ae7b8c45ad9a5f142d0
VigiePirate <artefact@vigies-pirates.net>
2021-09-05 16:51:39 +0200

    coat

--- src/Controller/CoatsController.php
+++ src/Controller/CoatsController.php
@@ -37,7 +37,7 @@ class CoatsController extends AppController
         $examples = $this->Coats->Rats->find('list', [
             ['order' => 'rand()'],
             ['limit' => '5']])
-            ->where(['Rats.Coats' => $id])
+            ->where(['coat_id' => $id])
             ->toArray();
         $this->set(compact('coat','examples'));
     }
