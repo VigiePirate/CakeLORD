fca070736d249a404081013b76b57c1f226774c8
VigiePirate <artefact@vigies-pirates.net>
2021-09-05 19:48:35 +0200

    Use staff sidebar element

--- templates/Ratteries/view.php
+++ templates/Ratteries/view.php
@@ -36,20 +36,11 @@
             </div>
 
             <div class="side-nav-group">
-                <div class="tooltip-staff">
-                    <?= $this->Html->image('/img/icon-edit-as-staff.svg', [
-                        'url' => ['controller' => 'Ratteries', 'action' => 'edit', $rattery->id],
-                        'class' => 'side-nav-icon',
-                        'alt' => __('Edit Rattery as Admin')]) ?>
-                    <span class="tooltiptext-staff"><?= __('Edit rattery data as staff') ?></span>
-                </div>
-
-                <div class="tooltip-staff">
-                    <?= $this->Html->image('/img/icon-delete.svg', [
-                        'class' => 'side-nav-icon',
-                        'alt' => __('Delete Rattery')]) ?>
-                    <span class="tooltiptext-staff"><?= __('Delete rattery') ?></span>
-                </div>
+                <?= $this->element('staff_sidebar', [
+                    'controller' => 'Ratteries',
+                    'object' => $rattery
+                    ])
+                ?>
             </div>
         </div>
     </aside>
