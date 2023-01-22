212473b449615003e13b170257f38246f82c42b2
VigiePirate <artefact@vigies-pirates.net>
2023-01-22 17:13:50 +0100

    Register form appearance and translation

--- templates/Users/register.php
+++ templates/Users/register.php
@@ -1,16 +1,16 @@
 <?= $this->Flash->render() ?>
 
 <div class="users form index content">
-    <h1>Register</h1>
+    <h1><?= __('Register') ?></h1>
     <?= $this->Form->create() ?>
     <fieldset>
         <legend><?= __('In order to create your account, please fill in this form') ?></legend>
-        <?= $this->Form->control('username', ['required' => true]) ?>
-        <?= $this->Form->control('email', ['required' => true]) ?>
-        <?= $this->Form->control('password', ['required' => true]) ?>
+        <?= $this->Form->control('username', ['label' => __('Username'), 'required' => true]) ?>
+        <?= $this->Form->control('email', ['label' => __('Email address'), 'required' => true]) ?>
+        <?= $this->Form->control('password', ['label' => __('Password'), 'required' => true]) ?>
         <legend><?= __('What is the meaning of the letter "R" in the LORD acronym?') ?></legend>
         <?= $this->Form->control('captcha', ['required' => true]) ?>
     </fieldset>
-    <?= $this->Form->submit(__('Register')); ?>
+    <?= $this->Form->button(__('Register')); ?>
     <?= $this->Form->end() ?>
 </div>
