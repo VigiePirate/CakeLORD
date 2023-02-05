d2f27f00fb1fef117519da6b94c7172a0f29fd0c
VigiePirate <artefact@vigies-pirates.net>
2023-02-05 20:32:07 +0100

    Litter edit skeleton

--- templates/Litters/edit.php
+++ templates/Litters/edit.php
@@ -7,35 +7,83 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <?= $this->element('default_sidebar') ?>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $litter->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $litter->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Litters'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->element('tech_sidebar', [
+                    'controller' => 'Litters',
+                    'object' => $litter,
+                    'tooltip' => 'Browse Litter List',
+                    'can_cancel' => true,
+                    'show_staff' => $show_staff,
+                    'user' => $user
+                ])
+            ?>
         </div>
     </aside>
     <div class="column-responsive column-90">
         <div class="litters form content">
-            <?= $this->Form->create($litter) ?>
+            <div class="sheet-heading">
+                <div class="sheet-title pretitle"><?= __('Litters') ?></div>
+            </div>
+            <h1><?=__('Edit litter #') . $litter->id ?></h1>
+            <?= $this->Form->setValueSources(['context', 'data'])->create($litter) ?>
+
+            <?php if (! $user->can('staffEdit', $litter)) : ?>
+                <div class="message warning">
+                    <?= __('In order to ensure data consistence, modifications of litter data are restricted. Please, contact a staff member if you want to edit the litter’s parents or birth date.') ?>
+                </div>
+            <?php endif ; ?>
+
             <fieldset>
-                <legend><?= __('Edit Litter') ?></legend>
                 <?php
                     echo $this->Form->control('mating_date', ['empty' => true]);
-                    echo $this->Form->control('birth_date');
-                    echo $this->Form->control('pups_number');
-                    echo $this->Form->control('pups_number_stillborn');
-                    echo $this->Form->control('comments');
-                    echo $this->Form->control('creator_user_id', ['options' => $users]);
-                    echo $this->Form->control('state_id', ['options' => $states]);
-                    echo $this->Form->control('parent_rats._ids', ['options' => $parentRats]);
-                    #echo $this->Form->control('ratteries._ids', ['options' => $ratteries]);
-                    echo $this->Form->control('contributions._ids', ['options' => $contributions]);
+
+                    echo $this->Form->control('pups_number', ['label' => __('Born-alive pups count')]);
+                    echo $this->Form->control('pups_number_stillborn', ['label' => __('Stillborn pups count')]);
+
+                    echo $this->Form->control('comments', [
+                        'name' => 'comments',
+                        'label' => __('Comments'),
+                        'rows' => '5',
+                        "error" => [
+                            "escape" => false
+                        ]
+                    ]);
                 ?>
+                <?php if ($user->can('staffEdit', $litter)) : ?>
+                    <h2 class="staff"><?= __('Staff-only') ?></h2>
+
+                    <?php
+                        echo $this->Form->control('birth_date');
+                        echo $this->Form->control('creator_user_id', ['options' => $users]);
+                        echo $this->Form->control('state_id', ['options' => $states]);
+                    ?>
+                <?php endif ; ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
 </div>
+
+<!-- Easy MDE -->
+<?= $this->Html->css('easymde.css') ?>
+<script src="https://unpkg.com/easymde/dist/easymde.min.js"></script>
+
+<script>
+    var easyMDE = new EasyMDE({
+        minHeight: "20rem",
+        spellChecker: false,
+        inputStyle: "contenteditable",
+        nativeSpellcheck: true,
+        previewImagesInEditor: true,
+        promptURLs: true,
+        sideBySideFullscreen: false,
+        toolbar: [
+            "bold", "italic", "strikethrough", "|",
+            "unordered-list", "ordered-list", "table", "|",
+            "link", "|",
+            "side-by-side", "fullscreen", "preview", "|",
+            "guide"
+        ]
+    });
+    easyMDE.toggleSideBySide();
+</script>
