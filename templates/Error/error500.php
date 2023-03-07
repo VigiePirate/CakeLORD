2f3222a4c5f0f75d3042feb14507b034ffa912de
VigiePirate <artefact@vigies-pirates.net>
2023-03-07 11:39:43 +0100

    Bad endif in error500 template.

--- templates/Error/error500.php
+++ templates/Error/error500.php
@@ -20,21 +20,21 @@ if (Configure::read('debug')) :
     </p>
 <?php endif; ?>
 <?php if (!empty($error->params)) : ?>
-    <strong>SQL Query Params: </strong>
-    <?php Debugger::dump($error->params) ?>
+	    <strong>SQL Query Params: </strong>
+	    <?php Debugger::dump($error->params) ?>
 <?php endif; ?>
 <?php if ($error instanceof Error) : ?>
-    <strong>Error in: </strong>
-    <?= sprintf('%s, line %s', str_replace(ROOT, 'ROOT', $error->getFile()), $error->getLine()) ?>
+	    <strong>Error in: </strong>
+	    <?= sprintf('%s, line %s', str_replace(ROOT, 'ROOT', $error->getFile()), $error->getLine()) ?>
 <?php endif; ?>
 <?php
     echo $this->element('auto_table_warning');
 
     $this->end();
-endif;
 ?>
 
-<?php else :
+<?php
+else :
     $this->layout = 'fullpage';
     $cakeDescription = 'Livre des Origines du Rat Domestique';
     ?>
