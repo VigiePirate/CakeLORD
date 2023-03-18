9eea1b52ac4eaac792b2feab73f9b315ce41e5af
VigiePirate <artefact@vigies-pirates.net>
2023-03-18 12:54:09 +0100

    Micro-edit rats comments

---
+++ templates/Rats/edit_comment.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Rat $rat
+ */
+?>
+
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <div class="side-nav-group">
+                <?= $this->element('default_sidebar') ?>
+            </div>
+            <div class="side-nav-group">
+                <div class="tooltip">
+                    <?= $this->Html->image('/img/icon-back.svg', [
+                        'url' => ['controller' => 'Rats', 'action' => 'view', $rat->id],
+                        'class' => 'side-nav-icon',
+                        'alt' => __('Full Screen')]) ?>
+                        <span class="tooltiptext"><?= __('Back to rat sheet') ?></span>
+                </div>
+            </div>
+        </div>
+    </aside>
+    <div class="column-responsive column-90">
+        <div class="rats form content">
+            <div class="sheet-heading">
+                <div class="sheet-title pretitle"><?= __('Edit Comments of ') ?></div>
+                <div class="tooltip-state">
+                    <div class="current-statemark statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></div>
+                    <span class="tooltiptext-state hide-on-mobile"><?= h($rat->state->name) ?></span>
+                </div>
+            </div>
+
+            <h1><?= $rat->usual_name . ' (' . $rat->pedigree_identifier . ')' ?></h1>
+
+            <?php echo $this->Form->create($rat); ?>
+
+            <?php
+                echo $this->Form->control('comments', [
+                    'name' => 'comments',
+                    'label' => '',
+                    'value' => $rat->comments,
+                    'rows' => '5',
+                    "error" => [
+                        "escape" => false
+                    ]
+                ]);
+            ?>
+
+            <?= $this->Form->button(__('Save comment')); ?>
+            <?= $this->Form->end(); ?>
+        </div>
+    </div>
+</div>
+
+<?= $this->Html->css('statebar.css') ?>
+
+<!-- Easy MDE -->
+<?= $this->Html->css('easymde.css') ?>
+<script src="https://unpkg.com/easymde/dist/easymde.min.js"></script>
+
+<script>
+    var easyMDE = new EasyMDE({
+        minHeight: "20rem",
+        spellChecker: false,
+        inputStyle: "contenteditable",
+        nativeSpellcheck: true,
+        previewImagesInEditor: true,
+        promptURLs: true,
+        sideBySideFullscreen: false,
+        toolbar: [
+            "bold", "italic", "strikethrough", "|",
+            "unordered-list", "ordered-list", "table", "|",
+            "link", "|",
+            "side-by-side", "fullscreen", "preview", "|",
+            "guide"
+        ]
+    });
+    easyMDE.toggleSideBySide();
+</script>
