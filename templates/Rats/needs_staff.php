f39a900510289539c5a99ddf0a7e7731870a0880
VigiePirate <artefact@vigies-pirates.net>
2023-03-25 18:28:47 +0100

    Improve display and pagination of rat lists for staff

--- templates/Rats/needs_staff.php
+++ templates/Rats/needs_staff.php
@@ -10,11 +10,5 @@
         <div class="sheet-title pretitle"><?= __('Back office') ?></div>
     </div>
     <h1><?= __('Rat sheets needing staff action') ?></h1>
-    <?= $this->element('rats', [ //rats
-        'rubric' => __(''),
-        'exceptions' => [
-            'picture',
-            'pup_name',
-        ],
-    ]) ?>
+    <?= $this->element('staff_rats') ?>
 </div>
