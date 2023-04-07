c8f0da9d7be21e3d502117a3c8d9ecf46a6754c6
VigiePirate <artefact@vigies-pirates.net>
2023-04-07 21:09:28 +0200

    Replace user sidebar by staff sidebar in back-office

---
+++ templates/element/my/staffbar.php
@@ -0,0 +1,51 @@
+<div class="side-nav-group">
+    <div class="tooltip-staff">
+        <?= $this->Html->image('/img/icon-staff-report.svg', [
+            'url' => ['controller' => 'Issues', 'action' => 'index'],
+            'class' => 'side-nav-icon filter-staff',
+            'alt' => __('List all issues')]) ?>
+        <span class="tooltiptext-staff"><?= __('See all issues') ?></span>
+    </div>
+
+    <div class="tooltip-staff">
+        <?= $this->Html->image('/img/icon-staff-help.svg', [
+            'url' => isset($help_url) ? $help_url : ['controller' => 'Articles', 'action' => 'index'],
+            'class' => 'side-nav-icon',
+            'alt' => __('List all articles')]) ?>
+        <span class="tooltiptext-staff"><?= __('See all articles') ?></span>
+    </div>
+</div>
+
+<div class="side-nav-group">
+    <div class="tooltip-staff">
+        <?= $this->Html->image('/img/icon-staff-user.svg', [
+            'url' => ['controller' => 'Users', 'action' => 'index'],
+            'class' => 'side-nav-icon',
+            'alt' => __('List all users')]) ?>
+        <span class="tooltiptext-staff"><?= __('See all users') ?></span>
+    </div>
+
+    <div class="tooltip-staff">
+        <?= $this->Html->image('/img/icon-staff-rat.svg', [
+            'url' => ['controller' => 'Rats', 'action' => 'index'],
+            'class' => 'side-nav-icon',
+            'alt' => __('List all rats')]) ?>
+        <span class="tooltiptext-staff"><?= __('See all rats') ?></span>
+    </div>
+
+    <div class="tooltip-staff">
+        <?= $this->Html->image('/img/icon-staff-rattery.svg', [
+            'url' => ['controller' => 'Ratteries', 'action' => 'index'],
+            'class' => 'side-nav-icon',
+            'alt' => __('List all ratteries')]) ?>
+            <span class="tooltiptext-staff"><?= __('See all ratteries') ?></span>
+    </div>
+
+    <div class="tooltip-staff">
+        <?= $this->Html->image('/img/icon-staff-litter.svg', [
+            'url' => ['controller' => 'Litters', 'action' => 'index'],
+            'class' => 'side-nav-icon',
+            'alt' => __('List all litters')]) ?>
+        <span class="tooltiptext-staff"><?= __('See all litters') ?></span>
+    </div>
+</div>
