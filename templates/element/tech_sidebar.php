ef4ad65c4b04dd311b7a91da2719fddfb07c4158
VigiePirate <artefact@vigies-pirates.net>
2023-04-27 20:33:44 +0200

    List articles from user session

--- templates/element/tech_sidebar.php
+++ templates/element/tech_sidebar.php
@@ -15,7 +15,7 @@
             <?php endif; ?>
             <div class="tooltip">
                 <?= $this->Html->image('/img/icon-list.svg', [
-                    'url' => ['controller' => $controller, 'action' => 'index'],
+                    'url' => ['controller' => $controller, 'action' => 'all'],
                     'class' => 'side-nav-icon',
                     'alt' => __('List')]) ?>
                     <span class="tooltiptext"><?= $tooltip ?></span>
