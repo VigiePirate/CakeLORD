72b322848cb9db4c909858fb3e86e8ed1e52c88a
VigiePirate <artefact@vigies-pirates.net>
2023-05-08 18:11:31 +0200

    Hide numbers in pagination with css

--- templates/element/rats.php
+++ templates/element/rats.php
@@ -121,12 +121,11 @@
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
             <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <div class="hide-on-mobile"><?= $this->Paginator->numbers() ?></div>
+            <?= $this->Paginator->numbers() ?>
             <?= $this->Paginator->next(__('next') . ' >') ?>
             <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
-
 </div>
 <?php endif; ?>
