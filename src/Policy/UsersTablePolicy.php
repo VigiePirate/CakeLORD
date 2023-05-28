cb501330233ca92e46dfae923d9db7b60c86294f
VigiePirate <artefact@vigies-pirates.net>
2023-05-28 20:51:27 +0200

    Bugfix on users table policies

--- src/Policy/UsersTablePolicy.php
+++ src/Policy/UsersTablePolicy.php
@@ -5,12 +5,28 @@ namespace App\Policy;
 
 use App\Model\Table\UsersTable;
 use Authorization\IdentityInterface;
+use Authorization\Policy\BeforePolicyInterface;
 
 /**
  * Users policy
  */
-class UsersTablePolicy
+class UsersTablePolicy implements BeforePolicyInterface
 {
+    /**
+     * Open all for admin
+     *
+     * @param Authorization\IdentityInterface $user The user.
+     * @param App\Model\Table\UsersTable $resource
+     * @param ?? $action
+     * @return bool
+     */
+    public function before($user, $resource, $action)
+    {
+        if ($user->getOriginalData()->is_root) {
+            return true;
+        };
+    }
+
     /**
      * Check if $user can see lists of users
      *
