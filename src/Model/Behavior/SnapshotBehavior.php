b79a8492f4fe3cdac29871ebfb7b5793a13ddfbc
VigiePirate <artefact@vigies-pirates.net>
2023-08-15 13:23:19 +0200

    Add options to deal with associations in SnapshotBehavior

--- src/Model/Behavior/SnapshotBehavior.php
+++ src/Model/Behavior/SnapshotBehavior.php
@@ -88,7 +88,10 @@ class SnapshotBehavior extends Behavior
      */
     public function snapRestore(EntityInterface $entity, $snapshot_id)
     {
-        $this->table()->removeBehavior('Picture');
+        if ($this->table()->hasBehavior('Picture')) {
+            $this->table()->removeBehavior('Picture');
+        }
+
         if ($entity->set($this->snapLoad($entity, $snapshot_id), ['guard' => false])) {
             if ($this->table()->save($entity, ['checkRules' => false])) {
                 return $this->snapDelete($entity, $snapshot_id);
@@ -146,14 +149,14 @@ class SnapshotBehavior extends Behavior
      * @param Integer $snapshot_id
      * @return array|boolean
      */
-    public function snapCompare(EntityInterface $entity, $snapshot_id)
+    public function snapCompare(EntityInterface $entity, $snapshot_id, $options = [])
     {
         $snapshot = $this->SnapshotsTable->get($snapshot_id);
         if ($snapshot->{$this->config['entityField']} == $entity->id) {
             if ($snapshot_data = $this->snapLoad($entity, $snapshot_id)) {
-                $raw_entity = $this->table()->get($entity->id);
+                $raw_entity = $this->table()->get($entity->id, $options);
                 $entity_data = json_decode(json_encode($raw_entity), true);
-                return array_diff_assoc($snapshot_data, $entity_data);
+                return $this->arrayDiffAssocRecursive($snapshot_data, $entity_data);
             }
         }
         return false;
@@ -168,11 +171,11 @@ class SnapshotBehavior extends Behavior
      * @param Integer $snapshot_id
      * @return string
      */
-    public function snapCompareAsString(EntityInterface $entity, $snapshot_id)
+    public function snapCompareAsString(EntityInterface $entity, $snapshot_id, $options = [])
     {
-        if ($diff_array = $this->snapCompare($entity, $snapshot_id)) {
+        if ($diff_array = $this->snapCompare($entity, $snapshot_id, $options)) {
             foreach ($diff_array as $key => $value) {
-                if (is_scalar($value)) {
+                if (! is_array($value)) {
                     $diff_array[$key] = $key . ': ' . $value;
                 } else {
                     $collection = new Collection($value);
@@ -199,5 +202,37 @@ class SnapshotBehavior extends Behavior
             return implode(', ', array_keys($diff_array));
         }
         return false;
+    }
+
+    /**
+     * arrayDiffAssocRecursive
+     *
+     * Extends php array_diff_assoc to nested arrays
+     *
+     * @param EntityInterface $entity
+     * @param Array $array1
+     * @param Array $array2
+     * @return string
+     */
+    public function arrayDiffAssocRecursive($array1, $array2)
+    {
+        $difference = array();
+
+        foreach ($array1 as $key => $value) {
+            if (is_array($value)) {
+                if (!isset($array2[$key]) || !is_array($array2[$key])) {
+                    $difference[$key] = $value;
+                } else {
+                    $new_diff = $this->arrayDiffAssocRecursive($value, $array2[$key]);
+                    if (!empty($new_diff)) {
+                        $difference[$key] = $new_diff;
+                    }
+                }
+            } elseif (! array_key_exists($key, $array2) || $array2[$key] !== $value) {
+                $difference[$key] = $value;
+            }
+        }
+
+        return $difference;
     }
 }
