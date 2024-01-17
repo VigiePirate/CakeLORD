8c9b677e649f6484593589f399ec07ce35426e85
VigiePirate <artefact@vigies-pirates.net>
2024-01-17 20:37:57 +0100

    Simplify count of pending sheets on back-office

--- templates/element/simple_staff_litters.php
+++ templates/element/simple_staff_litters.php
@@ -1,49 +1,53 @@
-<table class="summary">
-              <thead>
-                  <tr>
-                      <th><?= __('State') ?></th>
-                      <th><?= __('Litter') ?></th>
-                      <th><?= __('Creator') ?></th>
-                      <th><?= __('Modified') ?></th>
-                      <th><?= __('Last message') ?></th>
-                      <th class="actions"><?= __('Actions') ?></th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <?php foreach ($litters as $litter): ?>
-                  <tr>
-                      <td><span class="statecolor_<?php echo h($litter->state_id) ?>"><?= h($litter->state->symbol) ?></span></td>
-                      <td><?= $this->Html->link($litter->full_name, ['controller' => 'Litters', 'action' => 'view', $litter->id]) ?></td>
-                      <td><?= $litter->has('user') ? $this->Html->link($litter->user->username, ['controller' => 'Users', 'action' => 'view', $litter->user->id]) : '' ?></td>
-                      <td><?= $litter->modified->i18nFormat('dd/MM/yyyy') ?></td>
-                      <td><?= ! empty($litter->litter_messages) ? mb_strimwidth($litter->litter_messages[0]->content, 0, 48, '...') : '' ?></td>
-                      <td class="actions">
-                          <span class="nowrap">
-                              <?php if (! is_null($litter->last_snapshot_id)) :?>
-                                  <?= $this->Html->image('/img/icon-diff.svg', [
-                                      'url' => ['controller' => 'LitterSnapshots', 'action' => 'diff', $litter->last_snapshot_id],
-                                      'class' => 'action-icon',
-                                      'alt' => __('Diff')])
-                                  ?>
-                              <?php endif; ?>
-                              <?php if (! is_null($identity) && $identity->can('delete', $litter)) : ?>
+<?php if ($litters->count()) : ?>
+    <div class="table-responsive">
+        <table class="summary">
+          <thead>
+              <tr>
+                  <th><?= __('State') ?></th>
+                  <th><?= __('Litter') ?></th>
+                  <th><?= __('Creator') ?></th>
+                  <th><?= __('Modified') ?></th>
+                  <th><?= __('Last message') ?></th>
+                  <th class="actions"><?= __('Actions') ?></th>
+              </tr>
+          </thead>
+          <tbody>
+              <?php foreach ($litters as $litter): ?>
+              <tr>
+                  <td><span class="statecolor_<?php echo h($litter->state_id) ?>"><?= h($litter->state->symbol) ?></span></td>
+                  <td><?= $this->Html->link($litter->full_name, ['controller' => 'Litters', 'action' => 'view', $litter->id]) ?></td>
+                  <td><?= $litter->has('user') ? $this->Html->link($litter->user->username, ['controller' => 'Users', 'action' => 'view', $litter->user->id]) : '' ?></td>
+                  <td><?= $litter->modified->i18nFormat('dd/MM/yyyy') ?></td>
+                  <td><?= ! empty($litter->litter_messages) ? mb_strimwidth($litter->litter_messages[0]->content, 0, 48, '...') : '' ?></td>
+                  <td class="actions">
+                      <span class="nowrap">
+                          <?php if (! is_null($litter->last_snapshot_id)) :?>
+                              <?= $this->Html->image('/img/icon-diff.svg', [
+                                  'url' => ['controller' => 'LitterSnapshots', 'action' => 'diff', $litter->last_snapshot_id],
+                                  'class' => 'action-icon',
+                                  'alt' => __('Diff')])
+                              ?>
+                          <?php endif; ?>
+                          <?php if (! is_null($identity) && $identity->can('delete', $litter)) : ?>
+                              <?= $this->Html->image('/img/icon-delete.svg', [
+                                  'url' => ['controller' => 'Litters', 'action' => 'delete', $litter->id],
+                                  'class' => 'action-icon',
+                                  'alt' => __('Delete Litter')
+                              ])?>
+                          <?php else :?>
+                              <span class="disabled">
                                   <?= $this->Html->image('/img/icon-delete.svg', [
-                                      'url' => ['controller' => 'Litters', 'action' => 'delete', $litter->id],
-                                      'class' => 'action-icon',
-                                      'alt' => __('Delete Litter')
-                                  ])?>
-                              <?php else :?>
-                                  <span class="disabled">
-                                      <?= $this->Html->image('/img/icon-delete.svg', [
-                                          'url' => '',
-                                          'class' => 'action-icon disabled',
-                                          'alt' => __('Delete Litter')])
-                                      ?>
-                                  </span>
-                              <?php endif ;?>
-                          </span>
-                      </td>
-                  </tr>
-                  <?php endforeach; ?>
-              </tbody>
-          </table>
+                                      'url' => '',
+                                      'class' => 'action-icon disabled',
+                                      'alt' => __('Delete Litter')])
+                                  ?>
+                              </span>
+                          <?php endif ;?>
+                      </span>
+                  </td>
+              </tr>
+              <?php endforeach; ?>
+          </tbody>
+        </table>
+    </div>
+<?php endif ; ?>
--- templates/element/simple_staff_rats.php
+++ templates/element/simple_staff_rats.php
@@ -1,51 +1,53 @@
-<div class="table-responsive">
-    <table class="summary">
-        <thead>
-                <th><?= __('State') ?></th>
-                <th><?= __('Identifier') ?></th>
-                <th><?= __('Usual name') ?></th>
-                <th><?= __('Owner') ?></th>
-                <th><?= __('Modified') ?></th>
-                <th><?= __('Last message') ?></th>
-                <th class="actions-title"><?= __('Actions') ?></th>
-        </thead>
-        <tbody>
-            <?php foreach($rats as $rat): ?>
-                <tr>
-                    <td><span class="statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></span></td>
-                    <td><?= $this->Html->link($rat->pedigree_identifier, ['controller' => 'Rats', 'action' => 'view', $rat->id]) ?></td>
-                    <td><?= h($rat->usual_name) ?></td>
-                    <td><?= $rat->has('owner_user') ? $this->Html->link($rat->owner_user->username, ['controller' => 'Users', 'action' => 'view', $rat->owner_user->id]) : '' ?></td>
-                    <td><?= h($rat->modified->i18nFormat('dd/MM/yyyy')) ?></td>
-                    <td><?= ! empty($rat->rat_messages) ? mb_strimwidth($rat->rat_messages[0]->content, 0, 48, '...') : '' ?></td>
-                    <td class="actions">
-                        <span class="nowrap">
-                            <?php if (! is_null($rat->last_snapshot_id)) :?>
-                                <?= $this->Html->image('/img/icon-diff.svg', [
-                                    'url' => ['controller' => 'RatSnapshots', 'action' => 'diff', $rat->last_snapshot_id],
-                                    'class' => 'action-icon',
-                                    'alt' => __('Diff')])
-                                ?>
-                            <?php endif; ?>
-                            <?php if (! is_null($user) && $user->can('delete', $rat)) : ?>
-                                <?= $this->Html->image('/img/icon-delete.svg', [
-                                    'url' => ['controller' => 'Rats', 'action' => 'delete', $rat->id],
-                                    'class' => 'action-icon',
-                                    'alt' => __('Delete Litter')
-                                ])?>
-                            <?php else :?>
-                                <span class="disabled">
-                                    <?= $this->Html->image('/img/icon-delete.svg', [
-                                        'url' => '',
-                                        'class' => 'action-icon disabled',
-                                        'alt' => __('Delete Litter')])
+<?php if ($rats->count()) : ?>
+    <div class="table-responsive">
+        <table class="summary">
+            <thead>
+                    <th><?= __('State') ?></th>
+                    <th><?= __('Identifier') ?></th>
+                    <th><?= __('Usual name') ?></th>
+                    <th><?= __('Owner') ?></th>
+                    <th><?= __('Modified') ?></th>
+                    <th><?= __('Last message') ?></th>
+                    <th class="actions-title"><?= __('Actions') ?></th>
+            </thead>
+            <tbody>
+                <?php foreach($rats as $rat): ?>
+                    <tr>
+                        <td><span class="statecolor_<?php echo h($rat->state_id) ?>"><?= h($rat->state->symbol) ?></span></td>
+                        <td><?= $this->Html->link($rat->pedigree_identifier, ['controller' => 'Rats', 'action' => 'view', $rat->id]) ?></td>
+                        <td><?= h($rat->usual_name) ?></td>
+                        <td><?= $rat->has('owner_user') ? $this->Html->link($rat->owner_user->username, ['controller' => 'Users', 'action' => 'view', $rat->owner_user->id]) : '' ?></td>
+                        <td><?= h($rat->modified->i18nFormat('dd/MM/yyyy')) ?></td>
+                        <td><?= ! empty($rat->rat_messages) ? mb_strimwidth($rat->rat_messages[0]->content, 0, 48, '...') : '' ?></td>
+                        <td class="actions">
+                            <span class="nowrap">
+                                <?php if (! is_null($rat->last_snapshot_id)) :?>
+                                    <?= $this->Html->image('/img/icon-diff.svg', [
+                                        'url' => ['controller' => 'RatSnapshots', 'action' => 'diff', $rat->last_snapshot_id],
+                                        'class' => 'action-icon',
+                                        'alt' => __('Diff')])
                                     ?>
-                                </span>
-                            <?php endif ;?>
-                        </span>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</div>
+                                <?php endif; ?>
+                                <?php if (! is_null($user) && $user->can('delete', $rat)) : ?>
+                                    <?= $this->Html->image('/img/icon-delete.svg', [
+                                        'url' => ['controller' => 'Rats', 'action' => 'delete', $rat->id],
+                                        'class' => 'action-icon',
+                                        'alt' => __('Delete Litter')
+                                    ])?>
+                                <?php else :?>
+                                    <span class="disabled">
+                                        <?= $this->Html->image('/img/icon-delete.svg', [
+                                            'url' => '',
+                                            'class' => 'action-icon disabled',
+                                            'alt' => __('Delete Litter')])
+                                        ?>
+                                    </span>
+                                <?php endif ;?>
+                            </span>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+<?php endif ; ?>
